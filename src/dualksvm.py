c552c7e39b6070266d108e8543d878c727860637
qdengpercy <contact.qdeng@gmail.com>
2014-05-14 20:54:04 -0400

    dual

--- src/dualksvm.py
+++ src/dualksvm.py
@@ -12,7 +12,7 @@ class DualKSVM:
 
     lmda: int, regularizer
     alpha:  array type, weights of kernel classifier
-    max_iter : int, maximal number of iteration
+    T : int, maximal number of iteration - 1
     C : anoter regularizer, essentially C=1/lmda*n
 
     kernel : string type,
@@ -31,7 +31,7 @@ class DualKSVM:
         self.dim = n
         self.gm = gm
         self.nsweep = nsweep
-        self.max_iter = nsweep * n
+        self.T = nsweep * n
         self.kernel = kernel
         self.obj = []
         self.nnz = []
@@ -51,54 +51,54 @@ class DualKSVM:
         variables
         """
         n = K.shape[0]
-        yKy = (y[:, np.newaxis] * K) * y[np.newaxis, :]
-        lip = np.diag(yKy)
+        yky = (y[:, np.newaxis] * K) * y[np.newaxis, :]
+        lip = np.diag(yky)
         L_t = np.max(lip)
         Q = 1
         D_t = Q * self.dim * self.c ** 2 / 2
-        sig = self._esti_std(yKy)
-        gamma = np.minimum(1.0 / (2 * L_t), D_t / sig * np.sqrt(float(self.dim) / self.max_iter))
-        theta = gamma
+        sig = self._esti_std(yky)
+        gamma = np.minimum(1.0 / (2 * L_t), D_t / sig * np.sqrt(float(self.dim) / (1 + self.T)))
+        eta = np.ones(self.T + 1)
+        eta *= gamma
         alpha = np.zeros(n)
-        Z = 0
-        theta_sum = np.zeros(self.max_iter + 2)  # accumulated weight
-        alpha_sum = np.zeros(n)
+        a_tilde = np.zeros(n)
+        delta = np.zeros(self.T + 2)
         uu = np.ones(n, dtype=int)
         # index of the active iteration for each coordinate, u[i] = t means the most recent update of
-        # ith coordinate is in the t round
-        print "gamma: " + str(gamma) + " theta: " + str(theta)
+        # ith coordinate is in the t-th round, t = 0,1,...,T
+        # print "gamma: " + str(gamma) + " eta: " + str(eta)
+        showtimes = 5
         t = 0
         for i in range(self.nsweep):
-            samp = np.random.choice(n, size=(n, self.batchsize))  # index of samples to compute stochastic coordinate gradient
+            # index of batch data to compute stochastic coordinate gradient
+            samp = np.random.choice(n, size=(n, self.batchsize))
             samp = range(n)
-            perm = np.random.permutation(n)  # index of sampled coordinate to update
+            # index of sampled coordinate to update
+            perm = np.random.permutation(n)
             for j in range(n):
                 # samp_ind = samp[j, :]
                 samp_ind = samp
                 var_ind = perm[j]
-                theta_sum[t + 1] = theta_sum[t] + theta
-                alpha_sum[var_ind] += (theta_sum[t] - theta_sum[(uu[var_ind] - 1)]) * alpha[var_ind]
-                subK = yKy[var_ind, samp_ind]
-                stoc_coor_grad = np.dot(subK, alpha[samp_ind])*float(n)/self.batchsize - 1
+                delta[t + 1] = delta[t] + eta[t]
+                subk = yky[var_ind, samp_ind]
+                stoc_coor_grad = np.dot(subk, alpha[samp_ind]) * float(n) / self.batchsize - 1
+                a_tilde[var_ind] += (delta[t + 1] - delta[uu[var_ind]]) * (t - uu[j] + 1) * alpha[j]
                 alpha[var_ind] = self._prox_mapping(stoc_coor_grad, alpha[var_ind], gamma)
                 uu[var_ind] = t + 1
-                Z += theta
-                t += 1
-            print "# of sweeps " + str(i)
-            tmp = alpha_sum + (theta_sum[t + 1] - theta_sum[(uu - 1)]) * alpha
+            if i % (self.nsweep / showtimes) == 0:
+                print "# of sweeps " + str(i)
+            tmp = alpha_sum + (eta_sum[t + 1] - eta_sum[(uu - 1)]) * alpha
             alpha_avg = tmp / Z
-            yHa = np.dot(yKy, alpha_avg)
+            yHa = np.dot(yky, alpha_avg)
             res = self.lmda * (0.5 * np.dot(alpha_avg, yHa) - alpha_avg.sum())
             self.obj.append(res)
             nnzs = (alpha_avg != 0).sum()
             self.nnz.append(nnzs)
             err = np.mean(yHa > 0)
             self.err_tr.append(err)
-
-
         # averaging
-        alpha_sum += (theta_sum[self.max_iter] - theta_sum[uu - 1]) * alpha
-        self.alpha = alpha_sum / Z
+        a_tilde += (delta[self.T + 1] - delta[uu]) * (self.T - uu + 1) * alpha
+        self.alpha = a_tilde / delta[self.T + 1]
 
     def plot_train_result(self):
         row = 2
